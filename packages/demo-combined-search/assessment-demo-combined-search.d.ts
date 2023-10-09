19e13e8dfae6ce44b8bbcbeff55c191f8e722908
Anna <anna.sydorova.job@gmail.com>
2023-10-09 08:08:25 +0300

    feat: create monorepo for React components

---
+++ packages/demo-combined-search/assessment-demo-combined-search.d.ts
@@ -0,0 +1,18 @@
+declare module "assessment-demo-combined-search" {
+  import React from "react";
+
+  interface Items {
+    id: string;
+    value: string;
+  }
+
+  interface SearchProps {
+    width?: number;
+    items: Items[];
+    color?: string;
+  }
+
+  const Search: React.FC<SearchProps>;
+
+  export default Search;
+}
