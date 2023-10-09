8c2431a46de56e4b68e5626848dd55d6d9551284
Anna <anna.sydorova.job@gmail.com>
2023-10-09 08:19:00 +0300

    feat: create monorepo for React components

---
+++ packages/demo-input/src/Input.tsx
@@ -0,0 +1,30 @@
+import { ChangeEvent } from "react";
+import "./index.css";
+
+interface InputProps {
+  type?: "text" | "email" | "password";
+  placeholder: string;
+  oninput?: (event: ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+  onfocus?: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Input = ({
+  type = "text",
+  placeholder,
+  oninput,
+  value,
+  onfocus,
+}: InputProps) => {
+  return (
+    <>
+      <input
+        type={type}
+        placeholder={placeholder}
+        onInput={oninput}
+        value={value}
+        onFocus={onfocus}
+      />
+    </>
+  );
+};
