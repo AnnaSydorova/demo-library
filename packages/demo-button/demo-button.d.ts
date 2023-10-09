f4eed5f1a31513fbd56c218942edea532b63a445
Anna <anna.sydorova.job@gmail.com>
2023-10-09 07:46:32 +0300

    feat: create monorepo for React components

---
+++ packages/demo-button/demo-button.d.ts
@@ -0,0 +1,15 @@
+declare module "demo-button" {
+  import React from "react";
+
+  interface ButtonProps {
+    primary?: boolean;
+    backgroundColor?: string;
+    size?: "small" | "medium" | "large";
+    label: string;
+    onClick?: () => void;
+  }
+
+  const Button: React.FC<ButtonProps>;
+
+  export default Button;
+}
